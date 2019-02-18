6dc20e05a37e569bffd53d6ccb52111fac0dd0a6
Richard Saethang <richard.saethang@gmail.com>
2019-02-18 10:32:23 -0800

    add dashboard actions and delete profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
@@ -24,6 +30,39 @@ export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
   };
+};
+
+//Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
 
 // Clear profile
